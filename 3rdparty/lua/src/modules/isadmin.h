250e8a35e6c9261fbd1ff573ed2b3d0a3e0b61f8
Oliver Schneider <oliver@assarbad.net>
2014-01-31 02:15:35 +0000

    Removing CVS keywords

--- 3rdparty/lua/src/modules/isadmin.h
+++ 3rdparty/lua/src/modules/isadmin.h
@@ -1,6 +1,5 @@
 #ifndef __ISADMIN_H_VER__
 #define __ISADMIN_H_VER__ 2012102414
-// $Id: isadmin.h,v 52e6657a08c7 2012/10/24 15:21:39 oliver $
 #if (defined(_MSC_VER) && (_MSC_VER >= 1020)) || defined(__MCPP)
 #pragma once
 #endif // Check for "#pragma once" support
