5ecf411cfbeb03b80502ea4054d6a08f657aa041
Oliver Schneider <oliver@assarbad.net>
2014-01-31 02:15:35 +0000

    Removing CVS keywords

--- 3rdparty/lua/src/modules/dbgprint.h
+++ 3rdparty/lua/src/modules/dbgprint.h
@@ -1,6 +1,5 @@
 #ifndef __DBGPRINT_H_VER__
 #define __DBGPRINT_H_VER__ 2012102423
-// $Id: dbgprint.h,v e885f176d70a 2012/10/25 14:16:05 oliver $
 #if (defined(_MSC_VER) && (_MSC_VER >= 1020)) || defined(__MCPP)
 #pragma once
 #endif // Check for "#pragma once" support
